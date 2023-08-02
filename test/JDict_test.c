a11a57de54e960ed312d9dd13b7626e2b53e0f35
hazukieq <hazukie@qq.com>
2023-08-02 23:50:52 +0800

    add jdict&jsha test fn

---
+++ test/JDict_test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../JDict.h"
+
+int main(void){
+	jdict_test();
+        return 0;
+}
+
---
+++ test/JSha256_test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../JSha256.h"
+
+int main(void){
+	jsha_test();
+        return 0;
+}
+
